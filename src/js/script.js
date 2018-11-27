9c433b8e9b21ff8826242f49a6c7f7a5031ae855
shanks09 <smaith2@uic.edu>
2018-11-26 18:10:40 -0600

    modularized the code

--- src/js/script.js
+++ src/js/script.js
@@ -1,66 +1,97 @@
 "use strict";
 
-d3.json("data/data.json", function(json) {
-    console.log("Reading JSON")
-    // console.log(json[0])
-    // console.log(json[0].xPos)
-    var count = 0;
-    var pass = 0;
-	var pass1 = 0;
-    var running = true;
-	var running1 = false;
-    //console.log(count + "Records processed");
+var json;
+var count = 0;
+var pass = 0;
+var passT = 0;
+var running = true;
+var runningT = false;
 
-    var width = 650
-    var height = width * 0.75;
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+var width = 650
+var height = width * 0.75;
+var mouse, raycaster;
+var scene, camera, light, renderer, pointCloud;
+var sceneT, cameraT, lightT, rendererT, pointTrajectory, pointSolid;
+
+readJSON();
+createInitialScene();
+createTrajectoryScene();
+createPointCloud();
+document.addEventListener("keydown", onArrowKeyDown, false);
+window.addEventListener( 'mousedown', onMouseDown, false );
+
+function readJSON() {
+    console.log("Reading JSON")
+    jQuery.ajax({
+        dataType: "json",
+        url: "data/data.json",
+        async: false,
+        success: function (data) {
+            json = data
+        }
+    });
+    console.log("Done!!")
+    console.log(json[0])
+}
+
+
+function createInitialScene() {
+    raycaster = new THREE.Raycaster();
+    raycaster.params.Points.threshold = 10;
+    mouse = new THREE.Vector2();
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
     camera.position.set(30,175,150);
     camera.lookAt(-10,-20,0);
 	
-    var light = new THREE.DirectionalLight( 0xffffff, 1.5);
+    light = new THREE.DirectionalLight( 0xffffff, 1.5);
     light.position.set(0,2,20);
     light.lookAt(0,0,0);
     camera.add(light);
     scene.add(camera);
 	
-	var renderer = new THREE.WebGLRenderer();
-
+	renderer = new THREE.WebGLRenderer();
     renderer.setSize( width, height );
-	document.getElementById("scene").appendChild( renderer.domElement );
-	
-	var scene1 = new THREE.Scene();
-    var camera1 = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );	
-	camera1.position.set(30,175,150);
-    camera1.lookAt(-10,-20,0);
-	
-	var light1 = new THREE.DirectionalLight( 0xffffff, 1.5);
-    light1.position.set(0,2,20);
-    light1.lookAt(0,0,0);
-    camera1.add(light1);
-    scene1.add(camera1);
-	
-	var renderer1 = new THREE.WebGLRenderer();
-    
-	renderer1.setSize( width, height );
-    document.getElementById("trajectory_scene").appendChild( renderer1.domElement );
-	
-	var myOptions1 = new THREE.OrbitControls(camera1, renderer1.domElement);
-    myOptions1.update();
-	
-	//<--------------Plotting the 3D molecules in space----------------------------------------------------------------------------------------------------------------------->
+    document.getElementById("scene").appendChild( renderer.domElement );
+    var myOptions = new THREE.OrbitControls(camera, renderer.domElement);
+    myOptions.enablePan = false;
+    myOptions.update();
+}
+function createTrajectoryScene() {
+    sceneT = new THREE.Scene();
+    cameraT = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+    cameraT.position.set(30,175,150);
+    cameraT.lookAt(-10,-20,0);
+
+    lightT = new THREE.DirectionalLight( 0xffffff, 1.5);
+    lightT.position.set(0,2,20);
+    lightT.lookAt(0,0,0);
+    cameraT.add(lightT);
+    sceneT.add(cameraT);
+
+    rendererT = new THREE.WebGLRenderer();
+
+    rendererT.setSize( width, height );
+    document.getElementById("trajectory_scene").appendChild( rendererT.domElement );
+
+    var myOptionsT = new THREE.OrbitControls(cameraT, rendererT.domElement);
+    myOptionsT.update();
+}
+
+function createPointCloud() {
     var vectors = []
     var m = new THREE.PointsMaterial({
         vertexColors: THREE.VertexColors,
         size: 0.2
     });
-	
+
     var g = new THREE.Geometry();
-    var pointCloud = THREE.Points();
-    
-	for(var i=0;i<json.length;i++) {
+    pointCloud = THREE.Points();
+
+    for(var i=0;i<json.length;i++) {
         g.vertices.push(new THREE.Vector3(json[i].xPos[0], json[i].yPos[0], json[i].zPos[0]));
-		vectors.push(new THREE.Vector3(json[i].xPos[0], json[i].yPos[0], json[i].zPos[0]));
+        vectors.push(new THREE.Vector3(json[i].xPos[0], json[i].yPos[0], json[i].zPos[0]));
         if(json[i].label[0] == 1)
             g.colors.push(new THREE.Color("rgb(49,130,189)"))
         if(json[i].label[0] == 3)
@@ -71,19 +102,100 @@ d3.json("data/data.json", function(json) {
     pointCloud = new THREE.Points(g, m);
     pointCloud.name = 'shanksCloud';
     scene.add(pointCloud);
+}
 
-    //var abc = pointCloud.geometry;
 
-    var myOptions = new THREE.OrbitControls(camera, renderer.domElement);
-    myOptions.enablePan = false;
-    myOptions.update();
-    
-	//const domEvents = new THREEx.DomEvents(camera, render.domElement);
-	/*domEvents.addEventListener(pointCloud, 'click', event=>{
-		console.log("true");
-	});*/
-	
-    document.addEventListener("keydown", onArrowKeyDown, false);
+function onMouseDown(e) {
+    mouse.x =  ( e.clientX / renderer.domElement.width  ) * 2 - 1;
+    mouse.y = -( e.clientY / renderer.domElement.height ) * 2 + 1;
+    raycaster.setFromCamera( mouse, camera );
+    var intersects = raycaster.intersectObject(pointCloud);
+    if (intersects.length > 0) {
+        //console.log( 'intersects', intersects );
+        // Points.js::raycast() doesn't seem to sort this correctly atm,
+        // but how many points are found depends on the threshold set
+        // on the raycaster as well
+        // intersects = intersects.sort(function (a, b) {
+        //     return a.distanceToRay - b.distanceToRay;
+        // });
+        var particle = intersects[0];
+        console.log('Particle Clicked ', particle);
+
+        createTrajectory()
+    }
+    else{
+        console.log('NO Particle Clicked');
+    }
+}
+
+function createTrajectory(){
+    sceneT.remove(pointTrajectory);
+    sceneT.remove(pointSolid);
+    var vectorsT = []
+    var mT = new THREE.PointsMaterial({
+        vertexColors: THREE.VertexColors,
+        size: 0.3
+    });
+
+    var gT = new THREE.Geometry();
+
+    for(var i=0;i<json.length;i++) {
+        if(json[i].label[0] == 3)
+    	{
+    		gT.vertices.push(new THREE.Vector3(json[i].xPos[0], json[i].yPos[0], json[i].zPos[0]));
+    		vectorsT.push(new THREE.Vector3(json[i].xPos[0], json[i].yPos[0], json[i].zPos[0]));
+            gT.colors.push(new THREE.Color("rgb(189,189,189)"));
+    	}
+    }
+    pointSolid = new THREE.Points(gT, mT);
+    pointSolid.name = 'solidParticlesForScene2';
+    sceneT.add(pointSolid);
+
+    rendererT.render( sceneT, cameraT );
+
+
+    renderT();
+    function renderT() {
+        sceneT.remove(pointTrajectory);
+        gT = new THREE.Geometry();
+        vectorsT = [];
+        mT = new THREE.PointsMaterial({
+        vertexColors: THREE.VertexColors,
+        size: 3
+        });
+        if(running == true) {
+            setTimeout(function() {
+                requestAnimationFrame(renderT);
+            }, 1000);
+            passT++;
+            for (var i = 0; i < passT; i++) {
+            	gT.vertices.push(new THREE.Vector3(json[10000].xPos[0], json[10000].yPos[0], json[10000].zPos[0]));
+            	vectorsT.push(new THREE.Vector3(json[10000].xPos[0], json[10000].yPos[0], json[10000].zPos[0]));
+            	gT.vertices[i].x = json[10000].xPos[passT];
+                gT.vertices[i].y = json[10000].yPos[passT];
+                gT.vertices[i].z = json[10000].zPos[passT];
+            	gT.colors.push(new THREE.Color("rgb(227,74,51)"));
+            }
+            pointTrajectory = new THREE.Points(gT, mT);
+            pointTrajectory.name = 'pointTrajectory';
+            sceneT.add(pointTrajectory);
+
+            rendererT.render( sceneT, cameraT );
+            if (passT >= 90)
+                passT = 0;
+            pointCloud.geometry.verticesNeedUpdate = true;
+            // pointTrajectory.geometry.verticesNeedUpdate = true;
+        }
+        else {
+
+            requestAnimationFrame(renderT);
+            rendererT.render( sceneT, cameraT );
+        }
+    }
+
+}
+
+
     function onArrowKeyDown(event) {
         var keyCode = event.which;
         if (keyCode == 49) {
@@ -95,59 +207,34 @@ d3.json("data/data.json", function(json) {
 			render();
         }
 		if (keyCode == 13) {
-            running1 = true;
+            runningT = true;
         }
         else  if (keyCode == 13) {
-            running1 = false;
+            runningT = false;
         }
     };
     render();
     function render() {
-        console.log("in1");
-		scene1.remove(pointTrajectory);
-		g1 = new THREE.Geometry();
-		vectors1 = [];
-        m1 = new THREE.PointsMaterial({
-        vertexColors: THREE.VertexColors,
-        size: 3
-		});
 		if(running == true) {
             setTimeout(function() {
                 requestAnimationFrame(render);
             }, 1000);
             pass++;
-            pass1++;
 			for (var i = 0; i < json.length; i++) {
                 pointCloud.geometry.vertices[i].x = json[i].xPos[pass];
                 pointCloud.geometry.vertices[i].y = json[i].yPos[pass];
                 pointCloud.geometry.vertices[i].z = json[i].zPos[pass];
             }
-			for (var i = 0; i < pass1; i++) {
-				g1.vertices.push(new THREE.Vector3(json[10000].xPos[0], json[10000].yPos[0], json[10000].zPos[0]));
-				vectors1.push(new THREE.Vector3(json[10000].xPos[0], json[10000].yPos[0], json[10000].zPos[0]));
-				g1.vertices[i].x = json[10000].xPos[pass1];
-                g1.vertices[i].y = json[10000].yPos[pass1];
-                g1.vertices[i].z = json[10000].zPos[pass1];
-				g1.colors.push(new THREE.Color("rgb(227,74,51)"));
-            }
-			pointTrajectory = new THREE.Points(g1, m1);
-			pointTrajectory.name = 'pointTrajectory';
-			scene1.add(pointTrajectory);
 			
 			renderer.render( scene, camera );
-			renderer1.render( scene1, camera1 );
             if (pass >= json[0].xPos.length)
                 pass = 0;
-			if (pass1 >= 90)
-                pass1 = 0;
             pointCloud.geometry.verticesNeedUpdate = true;
-			pointTrajectory.geometry.verticesNeedUpdate = true;
         }
         else {
 			
-            //requestAnimationFrame(render);
-            //renderer.render( scene, camera );
-			//renderer1.render( scene1, camera1 );
+            requestAnimationFrame(render);
+            renderer.render( scene, camera );
         }
 	}
 		
@@ -174,29 +261,4 @@ d3.json("data/data.json", function(json) {
         renderer.render( scene, camera );
     }, false);*/
 
-	
-	//<--------------Plotting the single molecular trajectory----------------------------------------------------------------------------------------------------------------------->
-    var vectors1 = []
-    var m1 = new THREE.PointsMaterial({
-        vertexColors: THREE.VertexColors,
-        size: 0.3
-    });
-	
-    var g1 = new THREE.Geometry();
-    var pointTrajectory;
-    var solidParticlesScene2;
-	
-	for(var i=0;i<json.length;i++) {
-        if(json[i].label[0] == 3)
-		{
-			g1.vertices.push(new THREE.Vector3(json[i].xPos[0], json[i].yPos[0], json[i].zPos[0]));
-			vectors1.push(new THREE.Vector3(json[i].xPos[0], json[i].yPos[0], json[i].zPos[0]));
-            g1.colors.push(new THREE.Color("rgb(189,189,189)"));
-		}
-	}
-    solidParticlesScene2 = new THREE.Points(g1, m1);
-    solidParticlesScene2.name = 'solidParticlesForScene2';
-    scene1.add(solidParticlesScene2);
-	
-	renderer1.render( scene1, camera1 );
-})
+// })
